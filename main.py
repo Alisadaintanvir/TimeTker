e08d81ae6e3f9c6296c659c2c5cc970132e88fb9
alisadaintanvir <alisadaintanvir@gmail.com>
2023-08-14 02:54:01 +0600

    updated version

--- main.py
+++ main.py
@@ -3,39 +3,36 @@ from tkinter import *
 
 PINK = "#e2979c"
 RED = "#e7305b"
-GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 
 
 is_running = False
 counter_value = 0
-timer = None
+updating_timer = False
 
 
 def start_counter():
-    global is_running
+    global is_running, updating_timer
     is_running = not is_running
     if is_running:
         start_button.config(text="Pause")
-        update_counter()
+        if not updating_timer:
+            updating_timer = True
+            update_counter()
     else:
-        start_button.config(text="Start")
-
-
-def pause_counter():
-    global is_running
-    is_running = False
+        start_button.config(text="Resume")
 
 
 def stop_counter():
     global is_running, counter_value
     counter_value = 0
-    update_counter()
     is_running = False
+    start_button.config(text="Start")
+    canvas.itemconfig(timer_text, text=f"00:00:00")
 
 
 def update_counter():
-    global counter_value
+    global counter_value, updating_timer
     min_count = math.floor(counter_value / 60)
     hour_count = math.floor(min_count / 60)
     sec_count = math.floor(counter_value % 60)
@@ -49,8 +46,11 @@ def update_counter():
 
     if is_running:
         counter_value += 1
+        print(counter_value)
         canvas.itemconfig(timer_text, text=f"{hour_count}:{min_count}:{sec_count}")
         window.after(1000, update_counter)
+    else:
+        updating_timer = False
 
 
 window = Tk()
@@ -68,7 +68,7 @@ start_button = Button(text="Start", height=2, width=15, bg=RED, fg="#fff",
 button_window = canvas.create_window(380, 300, window=start_button)
 
 stop_button = Button(text="Stop", height=2, width=15, bg=RED, fg="#fff",
-                          highlightthickness=0, borderwidth=1, command= stop_counter)
+                          highlightthickness=0, borderwidth=1, command=stop_counter)
 pause_window = canvas.create_window(380, 350, window=stop_button)
 
 window.mainloop()
